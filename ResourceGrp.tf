48ffca4f2d3c7f78da75c04f9663b8627008e5af
karuppaiahdurai <50461794+karuppaiahdurai@users.noreply.github.com>
2022-07-06 19:54:59 +0530

    Update ResourceGrp.tf

--- ResourceGrp.tf
+++ ResourceGrp.tf
@@ -10,6 +10,11 @@ terraform {
 provider "azurerm" {
   # Configuration options
   features{}
+  
+  subscription_id   = "993d9b2c-2b4b-4d50-905d-e57929f72837"
+  tenant_id         = "6c4b146c-50ac-41d7-aa7e-662ee2387ec4"
+  client_id         = "a00a41be-1bfc-4ed3-9730-0fec19595a96"
+  client_secret     = "riy8Q~-gOWEnbAkL9Qygq3fmsIEByLO6_ztKwc~z"
 }
 
 
